b6b28ac0955f4d35fe21114087d8277a835ce957
guglielmopiasenti <piasentiguglielmo@gmail.com>
2024-02-12 18:47:34 +0100

    api fix

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,13 +1,29 @@
 import axios from "axios";
 
-const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+const CURRENT_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const FORECAST_API_URL = "https://api.openweathermap.org/data/2.5/forecast";
 const API_KEY = "3273650f64275c06b4e93f1c01ac9523";
-export const fetchWeather = async (city) => {
+
+export const fetchCurrentWeather = async (city) => {
   try {
-    const response = await axios.get(`${API_URL}?q=${city}&appid=${API_KEY}`);
+    const response = await axios.get(
+      `${CURRENT_WEATHER_API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
     return response.data;
   } catch (error) {
-    console.error("Error fetching weather data", error);
+    console.error("Error fetching current weather data", error);
+    throw error;
+  }
+};
+
+export const fetchWeatherForecast = async (city) => {
+  try {
+    const response = await axios.get(
+      `${FORECAST_API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching weather forecast data", error);
     throw error;
   }
 };
