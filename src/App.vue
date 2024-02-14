6a5c801a4caa4ef419bd4793eb6f9d5882ad8a10
guglielmopiasenti <piasentiguglielmo@gmail.com>
2024-02-14 18:52:32 +0100

    Add Sidebar component and functionality

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import useSelectImage from './composables/useSelectImage';
 import useGroupByDay from './composables/useGroupByDay';
 
 import SearchBar from './components/SearchBar.vue';
+import Sidebar from './components/Sidebar.vue';
 
 
 const currentWeather = ref(null);
@@ -12,6 +13,9 @@ const weatherForecast = ref(null);
 const error = ref(null);
 const { selectImage } = useSelectImage();
 const { groupedForecast } = useGroupByDay(weatherForecast, selectImage);
+const isSidebarOpen = ref(false);
+const cities = ref([]);
+const selectedCity = ref(null);
 
 
 onMounted(() => {
@@ -29,23 +33,46 @@ const searchWeather = async (city) => {
 };
 
 
+const handleAddCity = (city) => {
+  if (!cities.value.includes(city)) {
+    cities.value.push(city);
+    fetchCurrentWeather(city);
+  }
+};
+
+const handleSelectCity = (city) => {
+  selectedCity.value = city;
+  fetchCurrentWeather(city);
+};
+
 
 </script>
 
 
 <template>
   <div class="antialiased bg-[url('public/background.jpg')] min-h-screen">
+    <!-- Sidebar Component -->
     <div class="relative">
       <!-- Background Overlay -->
       <div class="absolute inset-0 bg-black opacity-25 z-0 min-h-screen"></div>
       <div class="relative">
+        <Sidebar :cities="cities" :isOpen="isSidebarOpen" @add-city="handleAddCity" @select-city="handleSelectCity" />
         <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-10">
           <!-- header section -->
           <header class="flex items-center justify-center gap-5">
             <h1 class="text-4xl sm:text-6xl py-5 sm:py-10 text-center text-gray-200">WeatherWhiz</h1>
             <img class="max-h-8 md:max-h-14 mt-3" src="/logo.png" alt="">
           </header>
-          <SearchBar :defaultCity="'Rotterdam'" @search="searchWeather" />
+          <div class="flex items-center justify-end gap-5">
+            <button @click="isSidebarOpen = !isSidebarOpen"
+              class="p-2 bg-gray-200 hover:bg-gray-300 rounded-full shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-opacity-60">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            </button>
+            <SearchBar :defaultCity="'Rotterdam'" @search="searchWeather" />
+          </div>
 
           <!-- main section -->
           <main>
